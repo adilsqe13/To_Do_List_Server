3d51526c59a6870eddf7dfd4563b31a0d563aed7
adilsqe13 <adilsqe13@gmail.com>
2024-01-16 17:55:25 +0530

    c

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI;
+const Tasks = require('./models/Task');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -24,58 +25,41 @@ db.once('open', () => {
 });
 
 //GET - REQUEST
-app.get('/api/allTask', (req, res) => {
-  db.query('SELECT * FROM tasklist', (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error fetching tasks');
-    } else {
-      res.status(200).json(results);
-    }
-  });
+app.get('/.netlify/functions/server/api/allTask', async(req, res) => {
+   const tasks = await Tasks.find({});
+   res.status(200).json(tasks);
 });
 
 
 //POST - REQUEST
-app.post('/api/allTask', (req, res) => {
+app.post('/.netlify/functions/server/api/allTask', async(req, res) => {
   const { trimmedTask } = req.body;
-  db.query('INSERT INTO tasklist VALUES (?)', [trimmedTask], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error adding student');
-    } else {
-      res.json(trimmedTask);
-    }
-  });
+  const task = new Tasks({
+      task: trimmedTask
+    })
+  await task.save();
+  res.json({success:true});
 });
 
 
 //DELETE - REQUEST
-app.delete('/api/allTask/:task', (req, res) => {
-  const task = req.params.task;
-  db.query('DELETE FROM taskList WHERE tasks = ?', [task], (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error deleting student');
-    }
-  });
+app.delete('/.netlify/functions/server/api/allTask/:taskId', async(req, res) => {
+  const taskId = req.params.taskId;
+  await Tasks.deleteOne({_id: taskId});
+  res.json({success:true});
 });
 
 
 //DELETE - REQUEST  (FOR RESET-ALL APP)
-app.delete('/reset-data', (req, res) => {
-  return db.query('DELETE FROM tasklist', (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error deleting student');
-    }
-  });
+app.delete('/.netlify/functions/server/reset-data', async(req, res) => {
+  await Tasks.deleteMany({});
+  res.json({success:true});
 });
 
 
 //SERVER - LISTENING
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 module.exports.handler = serverless(app);
